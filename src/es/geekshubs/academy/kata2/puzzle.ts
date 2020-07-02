79cde3844ff40ac7c77af4e4562bebe1c7190f26
vicboma1 <victorbolinchesmarin@gmail.com>
2020-07-02 18:10:25 +0200

    Implementación

--- src/es/geekshubs/academy/kata2/puzzle.ts
+++ src/es/geekshubs/academy/kata2/puzzle.ts
@@ -1,3 +1,19 @@
+//Palindromo
+//Hace el reverso de la string de entrada
+//'' -> ''       (T)
+//a  -> a        (T)
+//bu -> ub       (F)
+//qwe -> ewq     (F)
+//aba -> aba     (T)
+//hola -> aloh   (F)
+//anna -> anna   (T)
+//civic -> civic (T)
+//kayak -> kayak (T)
 export function palindromo(input:string) : boolean {
-    return false;
+    var res = "";
+
+    for(var i = input.length - 1 ; i >= 0 ;i--) 
+        res = res + input.charAt(i);
+
+    return res === input;
 }
--- test/es/geekshubs/academy/kata2/suite.test.ts
+++ test/es/geekshubs/academy/kata2/suite.test.ts
@@ -2,14 +2,78 @@ import { palindromo } from "../../../../../src/es/geekshubs/academy/kata2/puzzle
 
 describe('Testing for kata2', function (){
 
-    test('Palindromo - Size: 0', function () {
+    test('Palindromo - Size: 0 | \"\" === \"\"', function () {
         //Arrange
-        var expected = null;
+        var expected = true;
         //Act
         var result = palindromo("");
         //Assert
         expect(result).toBe(expected);
     });
 
+
+    test('Palindromo - Size: 1 | a === a', function () {
+        //Arrange
+        var expected = true;
+        //Act
+        var result = palindromo("");
+        //Assert
+        expect(result).toBe(expected);
+    });
+
+    
+    test('Palindromo - Size: 2 | aa === aa', function () {
+        //Arrange
+        var expected = true;
+        //Act
+        var result = palindromo("aa");
+        //Assert
+        expect(result).toBe(expected);
+    });
+
+    test('Palindromo - Size: 2 | ab === ba', function () {
+        //Arrange
+        var expected = true;
+        //Act
+        var result = palindromo("ab");
+        //Assert
+        expect(result).toBe(expected);
+    });
+
+     test('Palindromo - Size: 2 | ba === ab', function () {
+        //Arrange
+        var expected = true;
+        //Act
+        var result = palindromo("ba");
+        //Assert
+        expect(result).toBe(expected);
+    });
+
+    test('Palindromo - Size: 2 | bb === bb', function () {
+        //Arrange
+        var expected = true;
+        //Act
+        var result = palindromo("bb");
+        //Assert
+        expect(result).toBe(expected);
+    });
+
+    test('Palindromo - Size: 3 |  aba === aba', function () {
+        //Arrange
+        var expected = true;
+        //Act
+        var result = palindromo("aba");
+        //Assert
+        expect(result).toBe(expected);
+    });
+
+    test('Palindromo - Size: 3 |  abc === cba', function () {
+        //Arrange
+        var expected = true;
+        //Act
+        var result = palindromo("aba");
+        //Assert
+        expect(result).toBe(expected);
+    });
 });
 
