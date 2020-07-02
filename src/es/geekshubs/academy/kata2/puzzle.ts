49a2503af07b0da56aa90637135c4bdf39514c28
vicboma1 <victorbolinchesmarin@gmail.com>
2020-07-02 16:30:53 +0200

    Typescript Template Project

---
+++ src/es/geekshubs/academy/kata2/puzzle.ts
@@ -0,0 +1,2 @@
+export function xxxxxx(yyyy:hhhh) : zzzzzz {
+}
---
+++ test/es/geekshubs/academy/kata2/suite.test.ts
@@ -0,0 +1,15 @@
+import { xxxxx } from "../../../../../src/es/geekshubs/academy/kata2/puzzle";
+
+describe('Testing for kata2', function (){
+
+    test('xxxxxx - Size: 0', function () {
+        //Arrange
+        var expected = null;
+        //Act
+        var result = xxxxxx("");
+        //Assert
+        expect(result).toBe(expected);
+    });
+
+});
+
